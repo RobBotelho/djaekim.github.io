337186621eebc569643c9ee099473203c8fff2e1
Dong Jae Kim <dong.j.kim@mail.mcgill.ca>
2018-02-05 09:24:24 -0800

    Update slideShow.js

--- botelho2/js/slideShow.js
+++ botelho2/js/slideShow.js
@@ -1,34 +1,84 @@
-             var url = ['images/2017/Group-Photo-2017.jpg', 'images/individual/Bacteria-fragmentation.png','images/a.png'];
-            var caption = ['Welcome to the Botelho Lab!', 'ome text that describes me lorem ipsum ipsum lorem','ome text that describes me lorem ipsum ipsum lorem'];
+
+            var url = [
+            	'images/2017/Group-Photo-2017.jpg',
+            	 'images/Bacteria-fragmentation.png',
+            	'images/newspaper.jpg'
+            ]
+            var time;
             var index = 0;
-            setInterval(function(){
-               index = (++index)%3;
-               if (index == 0){
-               		$('#header2').fadeOut(2000);
-               } else if (index == 1){
-               		$('#header0').fadeOut(2000);
-               } else if (index == 2){
-               		$('#header1').fadeOut(2000);
-               }
-             
-               $('.fade ').fadeIn(2000);
-         
-              	setTimeout(function(){
-          		  $('.fade').fadeOut(2000);
-                   if (index == 0){
-               		$('#header0').fadeIn(2000);
-               		$('.fa-circle:eq(0)').css('color', '#25aca8');
-	               } else if (index == 1){
-	               		$('#header1').fadeIn(2000);
-               		$('.fa-circle:eq(1)').css('color', '#25aca8');
+            var currentOption;
+            Timer();
+            $('.slide a').click(function(){
+              var options = ($(this).attr('id'));
+              currentOption = options;
+              startTime();
 
-	               } else {
-	               		$('#header2').fadeIn(2000);
-               		$('.fa-circle:eq(2)').css('color', '#25aca8');
+     	      $('.slide a').not(options).each(function(){
+	              $(this).children().removeClass('fa fa-circle');
+	              $(this).children().addClass('fa fa-circle-o');
+              });
+              $('#'+options).children().addClass('fa fa-circle');
+              
+              $('.fade').fadeIn(500);
+              $('#header0').children().fadeOut(500);
+              setTimeout(function(){
+              	 
+              	 if (options.toString() == 'circleOne'){
+              	 	$('#main > section.one').css('background-image', 'url('+url[0]+')');
+              	 	$('#header-1').fadeIn();
+              	 	index = 0;
 
-	               }
-                  $('#main > section.one').css('background-image', 'url('+url[index]+')');
-              	},2000);
-                
-                 
-            }, 8000); 
+              	 } else if (options == 'circleTwo'){
+             	 	 $('#main > section.one').css('background-image', 'url('+url[1]+')');
+             	 	 $('#header-2').fadeIn();
+              	 	 index = 1;
+
+              	 } else if (options =='circleThree'){
+              		 $('#main > section.one').css('background-image', 'url('+url[2]+')');
+              		 $('#header-3').fadeIn();
+              	 	index = 2;
+
+              	 } 
+            	 $('.fade').fadeOut(500);
+
+              },500);
+            });
+            function Timer (){
+            	time = setInterval(function(){
+            	$('.fade').fadeIn(500);
+            	$('#header0').children().fadeOut(500);
+            	 index = (++index % 3);
+            	 setTimeout(function(){
+            	 	var option;
+            	 	$('#main > section.one').css('background-image', 'url('+url[index]+')');
+
+            	 	if (index == 0){
+            	 		option = 'circleOne';
+            	 		$('#header-1').fadeIn();
+
+
+            	 	} else if (index == 1){
+            	 		option = 'circleTwo';
+            	 		$('#header-2').fadeIn();
+
+            	 	} else {
+            	 		option = 'circleThree';
+            	 		$('#header-3').fadeIn();
+
+
+            	 	}
+	        	 	  $('.slide a').not(option).each(function(){
+			              $(this).children().removeClass('fa fa-circle');
+			              $(this).children().addClass('fa fa-circle-o');
+		              });
+	         		  $('#'+option).children().addClass('fa fa-circle');
+
+            	 	$('.fade').fadeOut(500);
+            	 },500);
+            }, 6000);
+            }
+            function startTime(){
+            	clearInterval(time);
+            	Timer();
+            }
+   
