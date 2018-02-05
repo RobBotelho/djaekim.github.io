fba45a6690c00f6103bbe00f96f0b4b270682dc4
Dong Jae Kim <dong.j.kim@mail.mcgill.ca>
2018-02-05 09:26:09 -0800

    Update responsive.js

--- botelho2/js/responsive.js
+++ botelho2/js/responsive.js
@@ -1,17 +1,8 @@
-               $('.motive').click(function() {
-                   if ($(this).next().css('display', 'none')) {
-                       $(this).next().slideDown(1000);
-                       $('html,body').animate({
-                               scrollTop: $(this).offset().top
-                           },
-                           250);
-
-                   }
-               });
+         
                $('.fa-times').click(function() {
                    $(this).parent().slideUp(250);
                });
-               $('.topic, .crew-topic, .topic-media').click(function() {
+               $('.topic, .crew-topic, .topic-media, .motive').click(function() {
                    var Top = $(this);
 
                    if ($(this).next().is(':visible')) {
